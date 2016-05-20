c6647a15953cf50debf30955957513a94543707b
MaartenBrijker <maartenbrijker@gmail.com>
2016-05-20 14:18:53 +0200

    initial, shows a NASA picture of the day

---
+++ ASOD9/ASODViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ASODViewController.swift
+//  ASOD9
+//
+//  Created by Maarten Brijker on 19/05/16.
+//  Copyright © 2016 Maarten_Brijker. All rights reserved.
+//
+
+import UIKit
+
+class ASODViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    
+    
+    
+ 
+
+}
